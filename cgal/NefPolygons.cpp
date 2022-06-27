93f4f5215285d341678e28208e9aa4216132e88e
Ryan Ketzner <rketzner@knights.ucf.edu>
2022-06-26 17:28:09 -0700

    Added explanatory comments to CGAL test file

--- cgal/NefPolygons.cpp
+++ cgal/NefPolygons.cpp
@@ -28,11 +28,6 @@ std::vector<Sphere_point> RvectorToSpherePoint(const std::vector<Rvector3> &rvec
         double z = rvectors[i].Get(2);
         Point_3 pointy(x,y,z);
 
-        // std::cout << "Vertex: " << i << std::endl; 
-        // std::cout << x  << std::endl;
-        // std::cout << y << std::endl;
-        // std::cout << z << std::endl;
-
         spherepoints.push_back(Sphere_point(pointy.x(),pointy.y(),pointy.z()));
     }
     return spherepoints;
@@ -42,53 +37,69 @@ std::vector<Sphere_segment> SpherePointToSphereSegment(const std::vector<Sphere_
 {
     std::vector<Sphere_segment> spheresegments;
 
-    // Last point is contained point, not needed in cgal
+    // The last point in the input is "contained point", which is not needed in cgal.
+    // The second to last point in the input is the first point repeated.
+    // Neither of these points are needed by CGAL, hence the iteration
+    // continues only up to points.size() - 2.
     for (int i = 0; i < (points.size()-2); i++)
     {
         spheresegments.push_back(Sphere_segment(points[i],points[i+1]));
     }
     
-    std::cout << "Number of points: " << points.size() << std::endl;
+    std::cout << "Number of points in input list: " << points.size() << std::endl;
     return spheresegments;
 }
 
 int main(int argc,char **argv)
 {
+    // The path to a polygon .csv file is specified as input to the program.
     std::string inputpoly = argv[1];
     std::vector<AnglePair> vertices = util::csvRead(inputpoly);
     std::vector<Rvector3> verticesxyz =  util::sphericalToCartesian(vertices);
     std::vector<Sphere_point> spherepoints = RvectorToSpherePoint(verticesxyz);
     std::vector<Sphere_segment> edges = SpherePointToSphereSegment(spherepoints);
     
-    Nef_polyhedron_S2 TN(edges.begin(),edges.end());
+    Nef_polyhedron_S2 input_poly(edges.begin(),edges.end());
 
-    // These operations cause crash
-    // Nef_polyhedron_S2 TN_int = TN.closure();
-    // Nef_polyhedron_S2 TN_reg = TN.regularization();
+    // These operations cause the porgram to crash.
+    // We were unable to determine the reason for this in the CGAL documentation.
+    // Nef_polyhedron_S2 input_poly_int = input_poly.closure();
+    // Nef_polyhedron_S2 input_poly_reg = input_poly.regularization();
 
+    // Get the centroid 
     Sphere_point centroid = spherepoints.back();
+    // Get the antipode of the centroid
     Sphere_point centroid_anti = centroid.antipode();
-    Object_handle location = TN.locate(centroid);
-    Object_handle location_anti = TN.locate(centroid_anti);
-    bool inside = TN.contains(location);
-    bool outside = TN.contains(location_anti);
+
+    // Run the CGAL point location algorithm on the centroid and antipode
+    Object_handle location = input_poly.locate(centroid);
+    Object_handle location_anti = input_poly.locate(centroid_anti);
+
+    // Determine whether these points are contained in the polygon
+    bool inside = input_poly.contains(location);
+    bool outside = input_poly.contains(location_anti);
 
     std::cout << "Centroid Contained: " << inside << std::endl;
     std::cout << "Centroid Antipode Contained: " << outside << std::endl;
     std::cout << "Centroid Value: " << centroid << std::endl;
     std::cout << "Centroid Antipode Value: " << centroid_anti << std::endl;
 
+    // Print the number of faces in the resulting sphere map
     std::cout << "Number of faces:" << std::endl;
-    std::cout << TN.number_of_sfaces() << std::endl;
+    std::cout << input_poly.number_of_sfaces() << std::endl;
 
+    // Print the number of vertices in the input polygon
     std::cout << "Number of vertices:" << std::endl;
-    std:: cout << TN.number_of_svertices() << std::endl;
+    std:: cout << input_poly.number_of_svertices() << std::endl;
 
     SVertex_const_handle sv;
     SHalfedge_const_handle se;
     SHalfloop_const_handle sl;
     SFace_const_handle sf;
 
+    // Will print the results of the CGAL point location algorithm run on
+    // the centroid of the input polygon. The output should tell the user 
+    // which type of component of the resulting sphere mape contains the point.
     if(CGAL::assign(sv,location))
         std::cout << "Locating svertex" << std::endl;
     else if(CGAL::assign(se,location))
@@ -102,10 +113,7 @@ int main(int argc,char **argv)
         return 1;
     }
 
-    TN.print_statistics(std::cout);
-
-    // std::cout << TN.number_of_connected_components() << std::endl;
-    // std::cout << TN.number_of_sedges() << std::endl;
+    input_poly.print_statistics(std::cout);
 
     return 0;
 }
